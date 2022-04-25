9e129d8a1de56f128b877250122a0d8286d47e54
ntk221 <nittakdub@gmail.com>
2022-04-25 19:01:58 +0900

    feat: division と multiplication 機能の実装テスト完了

--- go/money_test.go
+++ go/money_test.go
@@ -9,35 +9,48 @@ func TestMultiplicationInDollars(t *testing.T) {
 		amount:   5,
 		currency: "USD",
 	}
-	tenner := fiver.Times(2) // fiver に対するメソッド Times のテスト。もちろん、Times もまだ存在しない
-	if tenner.amount != 10 { // 実際の結果と、予想した値が一致しているか比較
-		t.Errorf("期待される答えは 10, got:[%d]", tenner.amount) // 実際の値と予想した値が一致しないときは test は失敗する
-	}
-	if tenner.currency != "USD" {
-		t.Errorf("期待される答えは USD, got:[%s]", tenner.currency)
-	}
+	actualResult := fiver.Times(2)                       // fiver.Times(2)の結果を変数 actualResult として宣言
+	expectedResult := Money{amount: 10, currency: "USD"} // expectedResultを変数として宣言
+
+	assertEqual(t, expectedResult, actualResult)
+
 }
 
 func TestMultiplicationInEuros(t *testing.T) {
 	tenEuros := Money{amount: 10, currency: "EUR"} // テストを書く段階では Money が未定義
-	twentyEuros := tenEuros.Times(2)
+	actualResult := tenEuros.Times(2)
+	expectedResult := Money{amount: 20, currency: "EUR"}
 
-	if twentyEuros.amount != 20 {
-		t.Errorf("期待されるのは 20 , got:[%d]", twentyEuros.amount)
-	}
-	if twentyEuros.currency != "EUR" {
-		t.Errorf("期待されるのは EUR , got:[%s]", twentyEuros.currency)
-	}
+	assertEqual(t, expectedResult, actualResult)
+}
+
+func TestDivision(t *testing.T) {
+	originalMoney := Money{amount: 4002, currency: "KRW"}                 // Division する前の Money を変数として宣言
+	actualMoneyAfterDivision := originalMoney.Divide(4)                   // originalMoney.Divide(4) した後の Money を変数として宣言
+	expectedMoneyAfterDivisison := Money{amount: 1000.5, currency: "KRW"} // originalMoney.Divide(4)の結果として期待される Money を変数として宣言
+
+	assertEqual(t, expectedMoneyAfterDivisison, actualMoneyAfterDivision)
 }
 
 // Money 構造体は通貨の種類とその量のフィールドを持つ
 type Money struct {
-	amount   int
+	amount   float64
 	currency string
 }
 
 // Money 構造体の amount に対して multiplier を掛けた結果を amount として更新した Money 構造体を返す.
 // currency フィールドは更新しない
 func (m Money) Times(multiplier int) Money {
-	return Money{amount: m.amount * multiplier, currency: m.currency}
+	return Money{amount: m.amount * float64(multiplier), currency: m.currency} // Money の amount フィールドが float64型 になるようにリファクタリングしたので、* の引数は float64 型でなくてはならない. 従って、multiplier は float64型にキャストする
+}
+
+func (m Money) Divide(divisor int) Money {
+	return Money{amount: m.amount / float64(divisor), currency: m.currency}
+}
+
+// Money 型の引数を二つとって、それらが等しいか否かアサーションテストする
+func assertEqual(t *testing.T, expected Money, actual Money) {
+	if expected != actual {
+		t.Errorf("期待される値は%+v Got %+v", expected, actual)
+	}
 }
--- js/test_money.js
+++ js/test_money.js
@@ -9,14 +9,22 @@ class Money {
     times(multiplier) {
         return new Money(this.amount * multiplier, this.currency);
     }
+
+    divide(divisor) {
+        return new Money(this.amount / divisor, this.currency) //JavaScript は動的型付け言語なので、go で必要になったキャストのリファクタは必要ない 
+    }
 }
 
-let fiver = new Money(5, "USD"); // USD 5 を表すオブジェクト、テストの段階で未定義
-let tenner = fiver.times(2); // fiverに対するメソッド
-assert.strictEqual(tenner.amount, 10); // 実際の値と期待される値がstrict equal であることのアサーション
-assert.strictEqual(tenner.currency, "USD");
+let fiveDollars = new Money(5, "USD");
+let tenDollars = new Money(10, "USD");
+assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
+
 
 let tenEuros = new Money(10, "EUR");
-let twentyEuros = tenEuros.times(2);
-assert.strictEqual(twentyEuros.amount, 20);
-assert.strictEqual(twentyEuros.currency, "EUR");
+let twentyEuros = new Money(20, "EUR");
+assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
+
+let originalMoney = new Money(4002, "KRW");
+let actualMoneyAfterDivision = originalMoney.divide(4);
+let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision); // assert.strictEqual を用いてオブジェクトのフィールドを個別にテストするのではなく、 assert.deepStrictEqual を用いて、オブジェクト単位で同じか（各フィールドの値が等しいか）テストしている
