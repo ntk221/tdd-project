58eeaee2f2c77e83f9b33de9d3751290fa5a84fc
ntk221 <nittakdub@gmail.com>
2022-04-25 16:20:48 +0900

    TDDサイクルの2回目終了

--- go/money_test.go
+++ go/money_test.go
@@ -4,24 +4,40 @@ import (
 	"testing"
 )
 
-func TestMultiplication(t *testing.T) {
-	fiver := Dollar{ // USD 5 を表す構造体、最初の failing test を書く段階ではまだ存在しない
-		amount: 5,
+func TestMultiplicationInDollars(t *testing.T) {
+	fiver := Money{ // USD 5 を表す構造体、最初の failing test を書く段階ではまだ存在しない
+		amount:   5,
+		currency: "USD",
 	}
 	tenner := fiver.Times(2) // fiver に対するメソッド Times のテスト。もちろん、Times もまだ存在しない
 	if tenner.amount != 10 { // 実際の結果と、予想した値が一致しているか比較
 		t.Errorf("期待される答えは 10, got:[%d]", tenner.amount) // 実際の値と予想した値が一致しないときは test は失敗する
 	}
+	if tenner.currency != "USD" {
+		t.Errorf("期待される答えは USD, got:[%s]", tenner.currency)
+	}
 }
 
-// fiver を宣言するために必要な Dollar 構造体の型
-type Dollar struct {
-	// amount フィールドを埋めるには、当然そのフィールドが必要である
-	amount int
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{amount: 10, currency: "EUR"} // テストを書く段階では Money が未定義
+	twentyEuros := tenEuros.Times(2)
+
+	if twentyEuros.amount != 20 {
+		t.Errorf("期待されるのは 20 , got:[%d]", twentyEuros.amount)
+	}
+	if twentyEuros.currency != "EUR" {
+		t.Errorf("期待されるのは EUR , got:[%s]", twentyEuros.currency)
+	}
 }
 
-func (d Dollar) Times(multiplier int) Dollar {
-	// return Dollar{10}
-	// return Dollar{5 * 2} // コード間の重複した部分を減らすことができた。
-	return Dollar{d.amount * multiplier} // 5 はd.amount 2 は multiplier のこと
+// Money 構造体は通貨の種類とその量のフィールドを持つ
+type Money struct {
+	amount   int
+	currency string
+}
+
+// Money 構造体の amount に対して multiplier を掛けた結果を amount として更新した Money 構造体を返す.
+// currency フィールドは更新しない
+func (m Money) Times(multiplier int) Money {
+	return Money{amount: m.amount * multiplier, currency: m.currency}
 }
--- js/test_money.js
+++ js/test_money.js
@@ -1,16 +1,22 @@
 const assert = require('assert'); // assert パッケージのインポート
 
-class Dollar{
-    constructor(amount){ // Dollar のコンストラクター として、int型の amount を引数として渡したら、Dollar の amount フィールドにセットするようにする。
+class Money {
+    constructor(amount, currency){
         this.amount=amount;
+        this.currency=currency;
     }
 
-    times(multiplier){
-        // return new Dollar(5 * 2);
-        return new Dollar(this.amount * multiplier); // ハードコーディングしていた箇所を修正
+    times(multiplier) {
+        return new Money(this.amount * multiplier, this.currency);
     }
 }
 
-let fiver = new Dollar(5); // USD 5 を表すオブジェクト、テストの段階で未定義
+let fiver = new Money(5, "USD"); // USD 5 を表すオブジェクト、テストの段階で未定義
 let tenner = fiver.times(2); // fiverに対するメソッド
 assert.strictEqual(tenner.amount, 10); // 実際の値と期待される値がstrict equal であることのアサーション
+assert.strictEqual(tenner.currency, "USD");
+
+let tenEuros = new Money(10, "EUR");
+let twentyEuros = tenEuros.times(2);
+assert.strictEqual(twentyEuros.amount, 20);
+assert.strictEqual(twentyEuros.currency, "EUR");
