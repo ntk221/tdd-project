bca957d19fab6b8dfdb5bfc72bbe1547a02dd33d
ntk221 <nittakdub@gmail.com>
2022-04-26 00:04:53 +0900

    feat: Moneysについてのaddition機能を Moneys の要素が同じ currency の時についてのみ実装

--- go/money_test.go
+++ go/money_test.go
@@ -32,10 +32,42 @@ func TestDivision(t *testing.T) {
 	assertEqual(t, expectedMoneyAfterDivisison, actualMoneyAfterDivision)
 }
 
+func TestAddition(t *testing.T) {
+	var portfolio Portfolio
+	var portfolioInDollars Money
+
+	fiveDollars := Money{amount: 5, currency: "USD"}
+	tenDollars := Money{amount: 10, currency: "USD"}
+	fifteenDollars := Money{amount: 15, currency: "USD"}
+
+	portfolio = portfolio.Add(fiveDollars)
+	portfolio = portfolio.Add(tenDollars)
+	portfolioInDollars = portfolio.Evaluate("USD") // currency を USD としてportfolioを評価
+
+	assertEqual(t, fifteenDollars, portfolioInDollars)
+}
+
 // Money 構造体は通貨の種類とその量のフィールドを持つ
 type Money struct {
 	amount   float64
 	currency string
+}
+
+// さまざまな currency の Money を要素として持つスライス
+type Portfolio []Money
+
+func (p Portfolio) Add(money Money) Portfolio {
+	p = append(p, money)
+	return p
+}
+
+func (p Portfolio) Evaluate(currency string) Money {
+	// return Money{amount: 15, currency: "USD"}
+	total := 0.0
+	for _, m := range p {
+		total = total + m.amount
+	}
+	return Money{amount: total, currency: currency} // 返り値の currency はEvaluate メソッドの第一引数にのみ依存しているため、この実装では不十分である
 }
 
 // Money 構造体の amount に対して multiplier を掛けた結果を amount として更新した Money 構造体を返す.
--- js/test_money.js
+++ js/test_money.js
@@ -15,6 +15,25 @@ class Money {
     }
 }
 
+class Portfolio{
+
+    constructor(){
+        this.moneys = [];
+    }
+
+    add(...moneys){ // money を複数引数としてとることを表す. rest parameter syntax
+        this.moneys = this.moneys.concat(moneys); // 引数にとった money の列を連結して,moneys フィールドに格納する
+    }
+
+    evaluate(currency) {
+        // return new Money(15, "USD"); // テストを通すためだけにハードコードしている(リファクタの段階で修正)
+        let total = this.moneys.reduce((sum, money) => { // reduce は配列 moneys に対して定義されている組み込みメソッドで、sum の初期値を 0 として、moneysの各要素の money を用いて計算を"畳み込んでいく". 関数型でいうと foldleft  
+            return sum + money.amount;
+        }, 0);
+        return new Money(total, currency);
+    }
+}
+
 let fiveDollars = new Money(5, "USD");
 let tenDollars = new Money(10, "USD");
 assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
@@ -28,3 +47,8 @@ let originalMoney = new Money(4002, "KRW");
 let actualMoneyAfterDivision = originalMoney.divide(4);
 let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
 assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision); // assert.strictEqual を用いてオブジェクトのフィールドを個別にテストするのではなく、 assert.deepStrictEqual を用いて、オブジェクト単位で同じか（各フィールドの値が等しいか）テストしている
+
+let fifteenDollars = new Money(15, "USD");
+let portfolio = new Portfolio();
+portfolio.add(fiveDollars, tenDollars);
+assert.deepStrictEqual(portfolio.evaluate("USD"), fifteenDollars); 
